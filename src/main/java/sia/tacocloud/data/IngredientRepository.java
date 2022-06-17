78c4162714f7ba3f8d09b91466bb15ee66c62bfb
nts931ag <ntson931@gmail.com>
2022-06-17 14:34:35 +0700

    spring data jpa

--- src/main/java/sia/tacocloud/data/IngredientRepository.java
+++ src/main/java/sia/tacocloud/data/IngredientRepository.java
@@ -1,9 +1,8 @@
 package sia.tacocloud.data;
 
+import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.Ingredient;
 
-public interface IngredientRepository {
-    Iterable<Ingredient> findAll();
-    Ingredient findOne(String id);
-    Ingredient save(Ingredient ingredient);
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+
 }
--- src/main/java/sia/tacocloud/data/OrderRepository.java
+++ src/main/java/sia/tacocloud/data/OrderRepository.java
@@ -3,6 +3,6 @@ package sia.tacocloud.data;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.Order;
 
-public interface OrderRepository  {
-    Order save(Order order);
+public interface OrderRepository extends CrudRepository<Order, Long>  {
+
 }
